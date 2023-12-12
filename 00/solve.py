3e274fcfb272a65ce0cc62dd60bb8ce1e07fbe07
LoggeL <hyper.xjo@gmail.com>
2023-12-12 19:44:14 +0100

    Template

--- 00/solve.py
+++ 00/solve.py
@@ -12,17 +12,19 @@ if __name__ == '__main__':
     test1_solution = 0
     test2_solution = 0
 
-    print("Test 1: " + str(solve1(test1)))
-    if test1_solution != solve1(test1):
+    test1_result = solve1(test1)
+    test2_result = solve2(test2)
+    
+    print("Test 1: " + str(test1_result))
+    if test1_solution != test1_result:
         print("Test 1 failed!")
         exit(1)
 
     print("Solution 1: " + str(solve1(input)))
     
-    print("Test 2: " + str(solve2(test2)))
-    if test2_solution != solve2(test2):
+    print("Test 2: " + str(test2_result))
+    if test2_solution != test2_result:
         print("Test 2 failed!")
         exit(1)
 
     print("Solution 2: " + str(solve2(input)))
-
